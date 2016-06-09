5bcbae9c96fb815231479d606fd1a0fbc90f8fca
Slashgear_ <antoine395.caron@gmail.com>
2016-06-09 14:18:26 +0200

    Add unit tests

--- test/app.js
+++ test/app.js
@@ -4,15 +4,34 @@ var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
 describe('generator-rancher-catalogue:app', function () {
+  const clusterTypes = ['swarm', 'mesos', 'kubernetes', 'cattle'];
+  const name = 'test';
   before(function () {
     return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true})
+      .withPrompts({
+        name: name,
+        clusterTypes: clusterTypes
+      })
       .toPromise();
   });
 
-  it('creates files', function () {
+  it('init git repository', function () {
     assert.file([
-      'dummyfile.txt'
+      '.git'
     ]);
+  });
+
+  it('init cluster template directory', function () {
+    console.log(clusterTypes);
+    clusterTypes.forEach(function (it) {
+      const prefix = it === 'cattle' ? '' : `${it}-`;
+      assert.file([
+        `${prefix}templates/${name}/0/docker-compose.yml`,
+        `${prefix}templates/${name}/0/rancher-compose.yml`,
+        `${prefix}templates/${name}/config.yml`,
+        `${prefix}templates/${name}/README.md`,
+        `${prefix}templates/${name}/yeoman-logo.svg`
+      ]);
+    });
   });
 });
